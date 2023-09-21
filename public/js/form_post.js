2d540bf4331dc62b38b28cb625103dc8e0f47baf
Brian Flores <brian.flores.ch@gmail.com>
2023-09-20 23:48:11 -0300

    Termina alta de usuario

---
+++ public/js/form_post.js
@@ -0,0 +1,13 @@
+/* http://www.institutomusicalhz.com.ar:3009/personal/nuevo */
+/* 
+    {
+nombre: “Jorge”,
+apellido: “Alton”,
+domicilio: “Terra 2020”,
+sector: “1”,
+cargo: “2”,
+newusername: “miusuario”,
+newpass: “miclave”
+};
+
+*/
